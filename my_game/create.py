b684eb5dbf2b4c0d3ac04dd471e3c1a1121ab0a1
gorbunova alice <gorbunova.av@phystech.edu>
2023-04-23 20:32:00 +0400

    Убрана папка my_game

--- my_game/create.py
+++
@@ -1,9 +0,0 @@
-#путь к файлам
-game_folder = os.path.dirname(__file__)
-
-#Создаем игру и окно
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((con.WIDTH, con.HEIGHT))
-pygame.display.set_caption("Морской бой")
-clock = pygame.time.Clock()
